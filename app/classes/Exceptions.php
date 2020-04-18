858bd72bb6877e0cb8db865100b046ec56163ffc
poljska <21zs7m8bcu@laposte.net>
2020-04-18 23:37:29 +0200

    Implement setShips method

--- app/classes/Exceptions.php
+++ app/classes/Exceptions.php
@@ -2,7 +2,14 @@
 
 class InvalidPlayer extends Exception {
     function __construct($player) {
-        $msg = $player . ': Error, player doesn\'t exist';
+        $msg = '[InvalidPlayer]['.$this->getFile().':'.$this->getLine().'] -- '.$player;
+        parent::__construct($msg);
+    }
+}
+
+class InvalidArguments extends Exception {
+    function __construct($args) {
+        $msg = '[InvalidArguments]['.$this->getFile().':'.$this->getLine().'] -- '.json_encode($args);
         parent::__construct($msg);
     }
 }
--- app/classes/Game.php
+++ app/classes/Game.php
@@ -19,10 +19,10 @@ final class Game {
         } else {  // New game
             $this->game_id = Uuid::uuid4()->toString();
             $this->timestamp = time();
-            $this->player_1_ships = '{}';
-            $this->player_1_shots = '[]';
-            $this->player_2_ships = '{}';
-            $this->player_2_shots = '[]';
+            $this->player_1_ships = array();
+            $this->player_1_shots = array();
+            $this->player_2_ships = array();
+            $this->player_2_shots = array();
             $this->status = array();
             $this->status['status'] = 'Ongoing';
             $this->status['turn'] = 'Player1';
@@ -46,10 +46,10 @@ final class Game {
         $return = array();
         $return['game_id'] = $this->game_id;
         $return['timestamp'] = $this->timestamp;
-        $return['player_1_ships'] = json_decode($this->player_1_ships);
-        $return['player_1_shots'] = json_decode($this->player_1_shots);
-        $return['player_2_ships'] = json_decode($this->player_2_ships);
-        $return['player_2_shots'] = json_decode($this->player_2_shots);
+        $return['player_1_ships'] = $this->player_1_ships;
+        $return['player_1_shots'] = $this->player_1_shots;
+        $return['player_2_ships'] = $this->player_2_ships;
+        $return['player_2_shots'] = $this->player_2_shots;
         $return['status'] = $this->status;
         return $return;
     }
@@ -63,19 +63,20 @@ final class Game {
     }
 
     public function getOngoingGame($player) {
+        if (!is_string($player)) throw new InvalidArguments($player);
         $return = array();
         $return['game_id'] = $this->game_id;
         $return['timestamp'] = $this->timestamp;
         $return['status'] = $this->status;
         switch ($player) {
             case 'player1':
-                $return['ships'] = $this->json_decode(player_1_ships);
-                $return['shots'] = $this->json_decode(player_1_shots);
+                $return['ships'] = $this->player_1_ships;
+                $return['shots'] = $this->player_1_shots;
                 break;
 
             case 'player2':
-                $return['ships'] = $this->json_decode(player_2_ships);
-                $return['shots'] = $this->json_decode(player_2_shots);
+                $return['ships'] = $this->player_2_ships;
+                $return['shots'] = $this->player_2_shots;
                 break;
 
             default:
@@ -85,11 +86,80 @@ final class Game {
         return $return;
     }
 
-    public function setShips($player, $positions) {
-        // TODO
+    public function setShips($player, $ships) {
+        if (!is_array($ships)) throw new InvalidArguments($ships);
+        if (count($ships) != 5) throw new InvalidArguments($ships);
+        if (!isset($ships['carrier'], $ships['battleship'], $ships['destroyer'],
+                   $ships['submarine'], $ships['patrol_boat'])
+           ) throw new InvalidArguments($ships);
+        foreach ($ships as $shipName => $shipPositions) {
+            if (!is_array($shipPositions)) throw new InvalidArguments($ships);
+            if (!Game::sizeGood($shipName, $shipPositions)) throw new InvalidArguments($ships);
+            if (!Game::validPositions($shipPositions)) throw new InvalidArguments($ships);
+            if (!Game::isContinuous($shipPositions)) throw new InvalidArguments($ships);
+        }
+        if (Game::overlap($ships)) throw new InvalidArguments($ships);
+        switch ($player) {
+            case 'player1':
+                $this->player_1_ships = $ships;
+                break;
+
+            case 'player2':
+                $this->player_2_ships = $ships;
+                break;
+
+            default:
+                throw new InvalidPlayer($player);
+                break;
+        }
     }
 
     public function fire($player, $position) {
         // TODO
+    }
+
+    private static function sizeGood($shipName, $shipPositions) {
+        $size = array('carrier' => 5, 'battleship' => 4, 'destroyer' => 3, 'submarine' => 3, 'patrol_boat' => 2);
+        return count($shipPositions) == $size[$shipName];
+    }
+
+    private static function validPositions($shipPositions) {
+        foreach ($shipPositions as $position) {
+            if (!Game::isPosition($position)) return FALSE;
+        }
+        return TRUE;
+    }
+
+    private static function isPosition($position) {
+        if (!is_array($position)) return FALSE;
+        if (count($position) != 2) return FALSE;
+        foreach ($position as $v) {
+            if (!is_int($v)) return FALSE;
+            if ($v < 1 || $v > 10) return FALSE;
+        }
+        return TRUE;
+    }
+
+    private static function isContinuous($shipPositions) {
+        sort($shipPositions);
+        $previousPosition = $shipPositions[0];
+        for ($i=1; $i < count($shipPositions); $i++) {
+            if (abs(($previousPosition[0] - $shipPositions[$i][0]) +
+                    ($previousPosition[1] - $shipPositions[$i][1])) != 1)
+                return FALSE;
+            $previousPosition = $shipPositions[$i];
+        }
+        return TRUE;
+    }
+
+    private static function overlap($ships) {
+        $allPosition = array();
+        foreach ($ships as $shipPositions) {
+            foreach ($shipPositions as $position) {
+                if (!in_array($position, $allPosition)) array_push($allPosition, $position);
+                else return TRUE;
+            }
+        }
+        return FALSE;
     }
 }
