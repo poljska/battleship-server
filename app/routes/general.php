3ea6e44dbc8455ea6d91e05a40ddba772c193478
poljska <21zs7m8bcu@laposte.net>
2020-04-25 06:37:51 +0200

    Implement NewGame endpoint

--- app/routes/general.php
+++ app/routes/general.php
@@ -1,6 +1,7 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+require_once 'app/classes/Game.php';
 
 /**
  * @api {get} /games Request a summary for all games
@@ -21,15 +22,35 @@ $app->get('/games', function (Request $request, Response $response, array $args)
  * @api {post} /games Create a new game
  * @apiName NewGame
  * @apiGroup General
+ * @apiPermission none
+ *
+ * @apiExample {curl} Example usage:
+ *      curl -X POST <domain>/games
+ *
+ * @apiSuccess (Success) {String} gameId Game ID
+ * @apiSuccessExample {json} Success response (example):
+ *      HTTP/1.1 201 Created
+ *      {"gameId":"9543fa9c-14a1-4494-83ac-d81196415c6d"}
+ *
+ * @apiErrorExample {json} Error response (example):
+ *      HTTP/1.1 500 Internal Server Error
  */
 $app->post('/games', function (Request $request, Response $response, array $args) {
-    $data = array('endpoint' => 'NewGame');
-    $payload = json_encode($data);
+    try {
+        $g = new Game();
+        $g->save();
+        $gameId = $g->getGameId();
+        $status = 201;
+        $payload = json_encode(array('gameId' => $gameId));
+    } catch (\Throwable $th) {
+        $status = 500;
+        $payload = '';
+    }
 
     $response->getBody()->write($payload);
     return $response
         ->withHeader('Content-Type', 'application/json')
-        ->withStatus(200);
+        ->withStatus($status);
 });
 
 /**
